6bfc5b47e9be9761ff87bf88334fb6679a65b367
Dennis Lehmann <delema98@gmail.com>
2022-04-19 23:15:34 +0200

    minor changes

--- Visualize_R/Visualization_Luna.R
+++ Visualize_R/Visualization_Luna.R
@@ -22,16 +22,18 @@ colors <- c("red", "blue", "green", "yellow", "grey", "orange",
 # die Zuordnung erfolgt durch den Index automatisch: 1 = rot, 2 = blau, ...
 
 
-# tiff("Aristotle.tiff", units="in", width = 10, height= 10 * 0.75, res=300)
+## Plot Erstellen ## 
+
+# png("Aristotle.png", units="in", width = 10, height= 10 * 0.75, res=300)
 
 par(mfrow = c(3, 4), mai = c(0.1, 0.1, 0.1, 0.1))
 
-
-for(i in 1:12){
+for(i in 1:12){ # erstellt fuer jede Loesung einen eigenen Plot
   l <- as.integer(unlist(strsplit(list[i], split = " ")))
   dat <- data.frame(x, y, z = l)
   dat <- dat[order(dat$z), ] # WICHTIG: Den Dataframe anhand der neuen Variable sortieren, 
-  # so stellt man sicher, dass die Farben immer den gleichen Nummern zugeordnet werden.
+  # so stellt man sicher, dass die Farben immer den gleichen Nummern zugeordnet werden, aber
+  # unterschiedliche Koordinaten haben
   plot(dat$x, dat$y, pch = 19, cex = 6.2, ylim = c(0, 6), xlim = c(0, 6), col = colors, 
        ylab = "", xlab = "", yaxt = "n", xaxt = "n", bty = "n")
   text(dat$x, dat$y, labels=1:19, cex=1.9, col = "white")
